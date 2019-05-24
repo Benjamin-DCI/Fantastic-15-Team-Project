c2365568edf4e855d23da39d46ecd626f06d06a1
Benjamin-DCI <47890043+Benjamin-DCI@users.noreply.github.com>
2019-05-24 18:49:10 +0200

    Initializing repository, first commit

---
+++ name-gen.js
@@ -0,0 +1,52 @@
+
+let name;
+
+switch (Math.floor(Math.random() * 15 + 1)) {
+  case 1:
+    name = "Katrin";
+    break;
+  case 2:
+    name = "Nizar";
+    break;
+  case 3:
+    name = "Richard";
+    break;
+  case 4:
+    name = "Salva";
+    break;
+  case 5:
+    name = "Trine";
+    break;
+  case 6:
+    name = "Tom";
+    break;
+  case 7:
+    name = "Dennis";
+    break;
+  case 8:
+    name = "Adrian";
+    break;
+  case 9:
+    name = "Arturo";
+    break;
+  case 10:
+    name = "Joseph";
+    break;
+  case 11:
+    name = "Lara";
+    break;
+  case 12:
+    name = "Vassia";
+    break;
+  case 13:
+    name = "Johnny";
+    break;
+  case 14:
+    name = "Leonard";
+    break;
+  case 15:
+    name = "Nike";
+    break;
+}
+
+console.log(name);
