0f58dc192a2da5bd7b4ec5a884f363ed4ac055d8
jsk900 <jsk900@yahoo.co.uk>
2019-05-25 06:01:52 +0200

    Additions

--- name-gen.js
+++ name-gen.js
@@ -1,52 +1,73 @@
+// let name;
 
-let name;
+// switch (Math.floor(Math.random() * 15 + 1)) {
+//   case 1:
+//     name = "Katrin";
+//     break;
+//   case 2:
+//     name = "Nizar";
+//     break;
+//   case 3:
+//     name = "Richard";
+//     break;
+//   case 4:
+//     name = "Salva";
+//     break;
+//   case 5:
+//     name = "Trine";
+//     break;
+//   case 6:
+//     name = "Tom";
+//     break;
+//   case 7:
+//     name = "Dennis";
+//     break;
+//   case 8:
+//     name = "Adrian";
+//     break;
+//   case 9:
+//     name = "Arturo";
+//     break;
+//   case 10:
+//     name = "Joseph";
+//     break;
+//   case 11:
+//     name = "Lara";
+//     break;
+//   case 12:
+//     name = "Vassia";
+//     break;
+//   case 13:
+//     name = "Johnny";
+//     break;
+//   case 14:
+//     name = "Leonard";
+//     break;
+//   case 15:
+//     name = "Nike";
+//     break;
+// }
 
-switch (Math.floor(Math.random() * 15 + 1)) {
-  case 1:
-    name = "Katrin";
-    break;
-  case 2:
-    name = "Nizar";
-    break;
-  case 3:
-    name = "Richard";
-    break;
-  case 4:
-    name = "Salva";
-    break;
-  case 5:
-    name = "Trine";
-    break;
-  case 6:
-    name = "Tom";
-    break;
-  case 7:
-    name = "Dennis";
-    break;
-  case 8:
-    name = "Adrian";
-    break;
-  case 9:
-    name = "Arturo";
-    break;
-  case 10:
-    name = "Joseph";
-    break;
-  case 11:
-    name = "Lara";
-    break;
-  case 12:
-    name = "Vassia";
-    break;
-  case 13:
-    name = "Johnny";
-    break;
-  case 14:
-    name = "Leonard";
-    break;
-  case 15:
-    name = "Nike";
-    break;
-}
+// console.log(name);
+
+let names = [
+  "Katrin",
+  "Nezar",
+  "Richard",
+  "Salva",
+  "Trine",
+  "Tom",
+  "Dennis",
+  "Adrian",
+  "Arturo",
+  "Joseph",
+  "Lara",
+  "Vassia",
+  "Johnny",
+  "Leonard",
+  "Nike"
+];
+
+let name = names[Math.floor(Math.random() * names.length)];
 
 console.log(name);
